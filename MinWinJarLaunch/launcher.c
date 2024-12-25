8fda0004f505f7a4e3f5191d89fb2f424c8708ac
Philipp <50533219+seerainer@users.noreply.github.com>
2024-12-25 21:41:55 +0100

    Update launcher.c

--- MinWinJarLaunch/launcher.c
+++ MinWinJarLaunch/launcher.c
@@ -11,17 +11,14 @@ WINAPI wWinMain(
     _In_ int cmdShow) {
 
     if (wcslen(szCmdLine) > 0) {
-
         intptr_t x = _wexeclp(L"javaw.exe", L"javaw.exe", L"-jar", szCmdLine, NULL);
 
         if (x < 0) {
-
             MessageBoxW(NULL, L"javaw.exe not found!", L"Error!", MB_ICONERROR | MB_OK);
 
             return EXIT_FAILURE;
         }
     } else {
-        
         MessageBoxW(NULL, L"No file specified!", L"Info!", MB_ICONEXCLAMATION | MB_OK);
 
         return EXIT_FAILURE;
